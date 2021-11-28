44bad302bdfaf6dab61d365b16c67da46552e218
Genhai <genhai@genhai.dev>
2021-11-28 14:45:26 +0800

    A mod of a keep fresh Krampus Sack

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dst-fresh-krampus-sack
 Don't Starve Together - Keep the freshness of the food in the Krampus Sack
+
+## Overview
+
+![](https://steamuserimages-a.akamaihd.net/ugc/1831272645886691885/5309F232FB65E2EC742E80C2AA48494499232177/?imw=200&imh=200&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true)
+
+## Changelog
+
+Created in 2021
---
+++ modinfo.lua
@@ -0,0 +1,17 @@
+name = "Fresh Krampus Sack"
+description = "Keep the freshness of the food in the Krampus Sack"
+author = "Genhai"
+version = "1.0.1"
+forumthread = "None"
+
+api_version = 10
+
+icon_atlas = "preview.xml"
+icon = "preview.tex"
+
+dst_compatible = true
+reign_of_giants_compatible = true
+all_clients_require_mod = true
+dont_starve_compatible = false
+
+server_filter_tags = {"items"}
